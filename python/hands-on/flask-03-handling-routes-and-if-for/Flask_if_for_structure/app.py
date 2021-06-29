581cd33bdcb0c9d0f7d70637b2ce4f829cfbc50d
altunozserkan <altunozserkan@gmail.com>
2021-06-29 22:24:23 +0300

    flask4

--- python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -1,25 +1,19 @@
-# Import Flask modules
 from flask import Flask, render_template
 
-# Create an object named app 
-app = Flask(__name__)
+app = Flask (__name__)
 
-# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
-# and assign to the route of ('/')
 @app.route('/')
 def head():
-    first = 'This is my first conditions experience'
-    return render_template('index.html', message = first)
+    first="This is my first condition experience"
+    return render_template("index.html",  message = False )
 
 
-# Create a function named header which prints numbers from 1 to 10 one by one in `index.html` 
-# and assign to the route of ('/')
-@app.route('/serkan')
+@app.route('/serdar')
 def header():
-    names = ['umut','berk','fatih','hayko', 'asım']
-    return render_template('body.html', object = names)
+    name = ["Serdar", "Fatih", "Ali", "Fatma", "Mostafa"]
+    return render_template("body.html", object = name)
 
-# run this app in debug mode on your local.
-if __name__ == '__main__':
+
+if __name__=="__main__":
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
-
